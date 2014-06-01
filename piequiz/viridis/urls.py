f071f16edc3bd4e88e4e30a4d98fb253dff48898
KenMutemi <mutemikennedy@gmail.com>
2014-06-01 17:19:38 +0300

    Create test page

--- piequiz/viridis/urls.py
+++ piequiz/viridis/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url, include
 from viridis import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<test_id>\d+)/(?P<slug>\w+)/$', views.test, name='test')
 )
--- piequiz/viridis/views.py
+++ piequiz/viridis/views.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import User
 def index(request):
     tests = Test.objects.order_by('-pub_date')
     return render(request, 'viridis/index.html', { 'tests': tests })
+
+def test(request, test_id, slug):
+    test = get_object_or_404(Test, pk=test_id)
+    return render(request, 'viridis/test.html', {'test': test})

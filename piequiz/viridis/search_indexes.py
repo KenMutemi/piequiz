73c43714c38f18436f14360158363ccda98f24ff
re <mutemikennedy@gmail.com>
2015-02-01 17:38:40 +0300

    Include questions in search page

--- piequiz/viridis/search_indexes.py
+++ piequiz/viridis/search_indexes.py
@@ -1,16 +1,27 @@
 import datetime
 from haystack import indexes
-from viridis.models import Test
+from viridis.models import Test, Question
 
 class TestIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    user = indexes.CharField(model_attr='user')
     pub_date = indexes.DateTimeField(model_attr='pub_date')
     content_auto = indexes.EdgeNgramField(model_attr='title') # For autocomplete
-   
+
     def get_model(self):
         return Test
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return Test.objects.filter(pub_date__lte=datetime.datetime.now())
+
+class QuestionIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    pub_date = indexes.DateTimeField(model_attr='pub_date')
+    content_auto = indexes.EdgeNgramField(model_attr='question_text') # For autocomplete
+
+    def get_model(self):
+        return Question
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return Question.objects.filter(pub_date__lte=datetime.datetime.now())
